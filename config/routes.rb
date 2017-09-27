97f38f5858c6cfd8b2e255bfbfd5f8d6ccc78f9f
Guillermo <gibsonguillermoarturo@gmail.com>
2017-09-27 16:21:13 -0300

    add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'comments/create'
 
   resources :posts do
   	resources :comments, only: [:create]
   end
 
+  get 'comments/create'
+
+  root 'posts#index'
 
 end
