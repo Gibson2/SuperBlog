2e520a74caa2b1004dc186f0fef5b1b9ea430c40
Guillermo <gibsonguillermoarturo@gmail.com>
2017-10-01 18:11:03 -0300

    Configuration of Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  def filter_admin!
+  	authenticate_user!
+  	redirect_to root_path, alert: "No tienes acceso"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/resistrations'
+  }
+
   resources :posts do
-  	resources :comments, only: [:create]
+    resources :comments, only: [:create]
   end
+
 
   get 'comments/create'
 
